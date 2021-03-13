e034f34eab8e0d7e597ae0d6ca8cc0d3752a5114
Zubareva Natalia <ndzubareva@edu.hse.ru>
2021-03-13 23:49:37 +0300

    app added

---
+++ FancyWork/app/src/main/java/com/example/fancywork/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fancywork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+///obviously we haven't done anything yet
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
