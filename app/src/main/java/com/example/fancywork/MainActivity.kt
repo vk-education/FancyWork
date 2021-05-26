8773a2c2049b2f7beab99d51c634f4f032cac153
Cortan122 <Cortan122@gmail.com>
2021-05-27 01:23:05 +0300

    Added image chooser and parsed the Bitmap

--- app/src/main/java/com/example/fancywork/MainActivity.kt
+++ app/src/main/java/com/example/fancywork/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.fancywork
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -7,13 +10,38 @@ import androidx.appcompat.app.AppCompatActivity
 
 // obviously we haven't done anything yet
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val PICK_IMAGE = 122
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+            val inputStream = this.applicationContext.contentResolver.openInputStream(data.data!!)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            // todo: create new Fragment/Activity and pass bitmap to it
+
+            Toast.makeText(
+                this.applicationContext,
+                "вы загрузили битмап ${bitmap.width}x${bitmap.height}!",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
     // todo for butten download image
     public fun download(view: View) {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+
         Toast.makeText(
             view.context,
             "вы ткнули на загрузку!",
