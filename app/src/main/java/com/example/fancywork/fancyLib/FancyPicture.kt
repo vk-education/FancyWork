af2e27b64787e22db244d93138cac9e3451bc439
Amina <afrizoeva@edu.hse.ru>
2021-05-27 00:21:08 +0300

    Add FancyPicture class

---
+++ app/src/main/java/com/example/fancywork/fancyLib/FancyPicture.kt
@@ -0,0 +1,23 @@
+package com.example.fancywork.fancyLib
+
+import android.graphics.Bitmap
+
+class FancyPicture (pair: Pair<Bitmap, Array<Array<String?>>> , title: String, id: String){
+
+    //todo id generator
+
+    var image: Bitmap = pair.first
+    var colors: Array<Array<String?>> = pair.second
+
+    var title: String = title
+    var id: String = id
+
+    var width: Int
+    var length: Int
+
+    init {
+        width = colors.size
+        length = colors[0].size
+    }
+
+}
