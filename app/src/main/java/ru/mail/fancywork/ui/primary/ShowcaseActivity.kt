03b88426cb889dbc00aeb08c9ebad642a2319e6e
Tigran <tigran.kocharyan@outlook.com>
2021-06-03 00:39:41 +0300

    Add share, fix backicon

--- app/src/main/java/ru/mail/fancywork/ui/primary/ShowcaseActivity.kt
+++ app/src/main/java/ru/mail/fancywork/ui/primary/ShowcaseActivity.kt
@@ -4,24 +4,21 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_showcase.*
-import kotlinx.android.synthetic.main.activity_workspace.*
 import kotlinx.coroutines.launch
 import ru.mail.fancywork.R
 import ru.mail.fancywork.controller.Controller
 import ru.mail.fancywork.model.datatype.Fancywork
 import ru.mail.fancywork.ui.secondary.ColorGridView
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
-import java.lang.Exception
 import kotlin.math.min
 
 class ShowcaseActivity : AppCompatActivity() {
@@ -37,20 +34,48 @@ class ShowcaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_showcase)
+        // TODO: FIX SHARE ICON APPEARANCE WITH NAV ICON
+        // setSupportActionBar(findViewById(R.id.top_bar_showcase))
 
-        setSupportActionBar(findViewById(R.id.top_bar))
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         colorGridView = findViewById(R.id.color_grid_view)
-
         fancywork = intent.getParcelableExtra(FANCYWORK_MESSAGE)!!
         val bmp = fancywork.bitmap
+
+        findViewById<Toolbar>(R.id.top_bar_showcase)
+            .setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.share -> {
+                        val intent = Intent(Intent.ACTION_SEND)
+                        intent.type = "image/png"
+                        val file = File(externalCacheDir, "temporary_file.png")
+                        try {
+                            if (!file.exists())
+                                file.createNewFile()
+                            val out = FileOutputStream(file)
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+                            out.close()
+                            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file))
+                            startActivity(Intent.createChooser(intent, "Share image"))
+                            true
+                        } catch (e: Exception) {
+                            Log.d("Share Exception", e.message.toString())
+                            false
+                        }
+                    }
+                    else -> false
+                }
+            }
+
         if (bmp != null) {
             bitmap = bmp
             colorGridView.setImage(bitmap, min(fancywork.height, fancywork.width))
         } else {
-            window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            )
             showcase_pb.visibility = View.VISIBLE
             showcase_view.visibility = View.VISIBLE
             lifecycleScope.launch {
@@ -69,23 +94,6 @@ class ShowcaseActivity : AppCompatActivity() {
             android.R.id.home -> {
                 finish()
                 true
-            }
-            R.id.share -> {
-                val intent = Intent(Intent.ACTION_SEND)
-                intent.type = "image/png"
-                val file = File(externalCacheDir, "temporary_file.png")
-                try {
-                    if (!file.exists())
-                        file.createNewFile()
-                    val out = FileOutputStream(file)
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
-                    out.close()
-                    intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file))
-                    startActivity(Intent.createChooser(intent, "Share image"))
-                    true
-                } catch (e: Exception) {
-                    false
-                }
             }
             else -> super.onOptionsItemSelected(item)
         }
